59909876044833bb532dea9173875e7c7e5fce25
mowalky <mowalky@outlook.com>
2022-10-21 07:44:49 -0500

    detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { experimental_use as use, Suspense } from "react";
+import { experimental_use as use, Suspense, useState } from "react";
 const idsFetch = fetch("/ids.json").then(async (res) => ({
   status: res.status,
   data: res.status === 200 ? await res.json() : null,
 }));
+
+const Detail = ({ id }) => {
+  return <div>{id}</div>;
+};
+
 const Names = () => {
   const ids = use(idsFetch);
+  const [selected, setSelected] = useState(null);
   return (
     <div>
-      {ids.data?.map((id) => (
-        <button key={id}>{id}</button>
+      {ids?.data.map((id) => (
+        <button onClick={() => setSelected(id)} key={id}>
+          Load {id}
+        </button>
       ))}
+      {selected && <Detail id={selected} />}
     </div>
   );
 };
