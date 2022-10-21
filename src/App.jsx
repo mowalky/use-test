8bb35f4b9e1ae8a7f34e68ec2a1b21795ebe74bc
mowalky <mowalky@outlook.com>
2022-10-21 07:38:53 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { experimental_use as use, Suspense } from "react";
-
+const idsFetch = fetch("/ids.json").then(async (res) => ({
+  status: res.status,
+  data: res.status === 200 ? await res.json() : null,
+}));
 const Names = () => {
-  const ids = use(fetch("/ids.json").then((res) => res.json()));
-  return <div>{JSON.stringify(ids)}</div>;
+  const ids = use(idsFetch);
+  return (
+    <div>
+      {ids.data?.map((id) => (
+        <button key={id}>{id}</button>
+      ))}
+    </div>
+  );
 };
 
 function App() {
